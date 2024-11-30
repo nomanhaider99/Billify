a780542497ba4f0276d7f2599a0063f821955024
Noman Haider <152289775+nomanhaider99@users.noreply.github.com>
2024-11-30 21:23:51 +0500

    sanity changes

--- app/actions/HomepageData.ts
+++ app/actions/HomepageData.ts
@@ -1,4 +1,4 @@
-import client from "@/lib/sanity";
+import { client } from "@/sanity/lib/client";
 import { HomepageTypes } from "@/types/data";
 
 const query = `{
--- app/actions/ServicesData.ts
+++ app/actions/ServicesData.ts
@@ -1,4 +1,4 @@
-import client from "@/lib/sanity";
+import { client } from "@/sanity/lib/client";
 import { ServicesData } from "@/types/data";
 
 const query = `*[_type == "service"][0]{

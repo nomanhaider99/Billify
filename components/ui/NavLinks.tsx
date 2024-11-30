0610b64b88d4420a2a3a78c589102bf8d3ec4ff2
Noman Haider <152289775+nomanhaider99@users.noreply.github.com>
2024-11-30 21:37:20 +0500

    image updated

--- components/ui/NavLinks.tsx
+++ components/ui/NavLinks.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { linksData } from '@/data/links'
 import Link from 'next/link'
 import React, { useState } from 'react'
@@ -6,7 +5,6 @@ import ServicesDrop from './ServicesDrop'
 import { IoIosArrowDown } from "react-icons/io";
 
 const NavLinks = () => { 
-    const [mouseEntered, setMouseEntered] = useState(false);
   return (
     <div className='w-full flex md:flex-row flex-col md:items-center gap-6 text-[16px] text-zinc-800'>
         {
@@ -18,15 +16,10 @@ const NavLinks = () => {
                     {item.link == 'Services' ? (
                         <div 
                             className='flex items-center gap-1'
-                            onMouseEnter={() => setMouseEntered(true)}
                         >
-                            {
-                                mouseEntered ? (
-                                    <ServicesDrop
-                                        tab='Services' 
-                                    />  
-                                ) : ''
-                            }
+                            <ServicesDrop
+                                tab='Services' 
+                            />
                             <IoIosArrowDown size={15} color='#000' />
                         </div>
                     ) : (item.link)}
